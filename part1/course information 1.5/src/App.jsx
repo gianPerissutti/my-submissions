0f9b8f1fd585da2fa9a7dfb58093b26af0c43a52
gianPeri <perissuttigianfranco@gmail.com>
2024-02-20 09:57:11 -0300

    course information add

---
+++ part1/course information 1.5/src/App.jsx
@@ -0,0 +1,34 @@
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((part,index) => (
+        <p key={index}>{part.name} {part.exercises}</p>
+      ))}
+    </div>
+  )
+
+}
+
+export default App
